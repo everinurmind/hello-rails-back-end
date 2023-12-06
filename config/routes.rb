887818491a195dc5756d6097761d9b86914abc4c
Nurbol <nurbol.sultanov@gmail.com>
2023-12-05 19:33:18 -0800

    Move greetings route to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  get 'greetings/random', to: 'greetings#random'
+  get '/', to: 'greetings#random'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
